8b2fa9436a959b00b00e918284f27b01f49b3302
JasonLu <raylupus@gmail.com>
2017-11-01 09:37:17 +1300

    sed ignore files

--- package.sh
+++ package.sh
@@ -1,13 +1,14 @@
 ROOT_FOLDER=`pwd`
+SED_IGNORE_FILES="magento/2.1.1/app/code/Magento5/Latipay/etc/config.xml|package.sh"
 LATIPAY_API=$1
 if [ "x$LATIPAY_API" = "x" ]; then
    echo "Package Plugin Connect Latipay 2.0 Production Environment"
    echo "Replace All api-staging.latipay.net To api.latipay.net"
-   echo sed -i "" "s/api-staging.latipay.net/api.latipay.net/g" `grep "api-staging.latipay.net" -rl $ROOT_FOLDER | grep -v package.sh`
+   echo sed -i "" "s/api-staging.latipay.net/api.latipay.net/g" `grep "api-staging.latipay.net" -rl $ROOT_FOLDER | grep -Ev "$SED_IGNORE_FILES"`
 else
    echo "Package Plugin Connect Latipay 2.0 Test Environment"
    echo "Replace All api.latipay.net To api-staging.latipay.net"
-   sed -i "" "s/api.latipay.net/api-staging.latipay.net/g" `grep "api.latipay.net" -rl $ROOT_FOLDER | grep -v package.sh`
+   sed -i "" "s/api.latipay.net/api-staging.latipay.net/g" `grep "api.latipay.net" -rl $ROOT_FOLDER | grep -Ev "$SED_IGNORE_FILES"`
 fi
 
 rm -rf target
