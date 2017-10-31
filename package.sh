cd3513f634e088efff1993db1baab1adf63e3a9b
JasonLu <raylupus@gmail.com>
2017-11-01 09:32:38 +1300

    package staging

--- package.sh
+++ package.sh
@@ -1,3 +1,15 @@
+ROOT_FOLDER=`pwd`
+LATIPAY_API=$1
+if [ "x$LATIPAY_API" = "x" ]; then
+   echo "Package Plugin Connect Latipay 2.0 Production Environment"
+   echo "Replace All api-staging.latipay.net To api.latipay.net"
+   echo sed -i "" "s/api-staging.latipay.net/api.latipay.net/g" `grep "api-staging.latipay.net" -rl $ROOT_FOLDER | grep -v package.sh`
+else
+   echo "Package Plugin Connect Latipay 2.0 Test Environment"
+   echo "Replace All api.latipay.net To api-staging.latipay.net"
+   sed -i "" "s/api.latipay.net/api-staging.latipay.net/g" `grep "api.latipay.net" -rl $ROOT_FOLDER | grep -v package.sh`
+fi
+
 rm -rf target
 mkdir target
 for plugin_folder in `ls`
@@ -7,9 +19,10 @@ do
       do
          if [ -d $plugin_folder/$version ]; then
             cd $plugin_folder/$version
-            zip -r ${plugin_folder}-${version}.zip *
+            zip -r ${plugin_folder}-${version}.zip * >/dev/null 2>&1
             mv ${plugin_folder}-${version}.zip ../../target/
-            cd -
+            cd $ROOT_FOLDER
+            echo "target/${plugin_folder}-${version}.zip"
          fi
       done
    fi
