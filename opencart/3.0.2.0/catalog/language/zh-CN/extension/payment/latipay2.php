35a08908957d574faf85795c4af8261303774cee
siqinglin <185328470@qq.com>
2018-05-30 03:02:56 +0800

    support opencart 3.0.2.0 fix image display error

--- opencart/3.0.2.0/catalog/language/zh-CN/extension/payment/latipay2.php
+++ opencart/3.0.2.0/catalog/language/zh-CN/extension/payment/latipay2.php
@@ -1,6 +1,6 @@
 <?php
 // Text
-$_['text_title'] = '微信支付|支付宝|京东支付|在线网银 <img src="image/catalog/payment/latipay2.png" width="204px" height="45px" />';
+$_['text_title'] = '微信支付|支付宝|京东支付|在线网银 <img src="/image/catalog/payment/latipay2.png" width="204px" height="45px" />';
 
 $_['text_payment_method'] = '付款方式 : ';
 
