04ce5d4d1af8695f535c18689ffc1b5b4a2c885d
linsq <185328470@qq.com>
2018-12-25 23:43:09 +0800

    add debug model

--- woocommerce/3.1.0/class-latipay-alipay-wc-payment-gateway.php
+++ woocommerce/3.1.0/class-latipay-alipay-wc-payment-gateway.php
@@ -1,14 +1,16 @@
 <?php
-if (! defined ( 'ABSPATH' ))
-	exit (); // Exit if accessed directly
-class XHLatipayAlipayForWC extends Abstract_XH_LATIPAY_Payment_Gateway {
-    public function __construct() {
-		parent::__construct();
-		
-	    $this->icon               = XH_LATIPAY_URL . '/images/alipay.png';
-		$this->method_title       = __('Latipay - Alipay',XH_LATIPAY);
-	}
-	
+if (!defined('ABSPATH'))
+    exit (); // Exit if accessed directly
+class XHLatipayAlipayForWC extends Abstract_XH_LATIPAY_Payment_Gateway
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->icon = XH_LATIPAY_URL . '/images/alipay.png';
+        $this->method_title = __('Latipay - Alipay', XH_LATIPAY);
+    }
+
     public function get_payment_method()
     {
         return 'alipay';
